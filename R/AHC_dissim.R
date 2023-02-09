a5be1bdd70b8b5a5d3fa76193fdb19fbaa1c5e48
TMargaté <margate.tristan@gmail.com>
2023-02-09 15:47:59 +0100

    update AHC.dissim

--- R/AHC_dissim.R
+++ R/AHC_dissim.R
@@ -1,47 +1,21 @@
 
-#' Naive classification by dissimilarity
+#' Title
 #'
-#' @param D dissimilarity matrix
-#' @param X covariate matrix (if you do not have the dissimilarity matrix, it will be calculated)
-#' @param method type of method used for the calculation of dissimilarities (e.g. "centroid", "median" etc...)
+#' @param D
+#' @param X
+#' @param method
 #'
 #' @return
 #' @export
-#' @import ggfortify
-#' @import ggplot2
 #'
-#' @examples methods = c("single","complete","average","weighted","centroid","median","Ward")
-#' X = as.matrix(iris[-5])
-#' iris.pca = prcomp(X,center=TRUE,scale.=TRUE)
-#' table.dissim = list()
-#'
-#' for (method in methods){
-#'   P = AHC.dissim(X=X,method=method)
-#'   P3 = P[150-length(levels(iris$Species))+1,]
-#'   table.dissim[[method]] = table(P3,iris$Species)
-#'   print(method)
-#'   print(table.dissim[[method]])
-#'   print(ggplot2::autoplot(iris.pca,data=iris,colour=P3)+ggplot2::ggtitle(method))
-#' }
-#' print(ggplot2::autoplot(iris.pca,data=iris,colour="Species")+ggplot2::ggtitle("true species"))
+#' @examples
 #'
 AHC.dissim = function(D=0,X=0,method="average"){
-  if (!method %in% c("single","complete","average","weighted","centroid","median","Ward")) stop("the method given is unknown")
-  if (!is.matrix(X) & !is.matrix(D)) stop("no values have been given to both D and X or the value(s) is (are) not a matrix")
-  else if (!is.matrix(D)){
-    n0 = dim(X)[1]
-    D = matrix(0,n0,n0)
-    for (j in 1:(n0-1)){
-      for (i in (j+1):n0){
-        D[i,j] = sum((X[i,]-X[j,])**2)
-        D[j,i] = D[i,j]
-      }
-    }
-  }
-  else{
-    if (!isSymmetric(D)) stop("D must be square symmetric")
+  if (!is.matrix(D)){
+    D = as.matrix(dist(X))
   }
   diag(D) = Inf
+  n0 = nrow(D)
   rownames(D) = 1:n0
   colnames(D) = 1:n0
   n0 = dim(D)[1]
@@ -61,8 +35,9 @@ AHC.dissim = function(D=0,X=0,method="average"){
   }
   P = matrix(0,n0,n0)
   P[1,] = 1:n0
+  t = rep(0,n0-1)
   n = n0
-  for (k in 2:n){
+  for (k in 2:n0){
     # On cherche les 2 classes les plus proches
     arg.min = which.min(D)
     j = (arg.min-1)%/%n+1
@@ -95,6 +70,7 @@ AHC.dissim = function(D=0,X=0,method="average"){
         }
       }
     }
+    t[k-1] = D[i,j]
     # On met à jour D
     D[j,] = alpha.i*D[i,]+alpha.j*D[j,]+beta*D[i,j]+gamma*abs(D[i,]-D[j,])
     D[j,j] = Inf
@@ -102,5 +78,5 @@ AHC.dissim = function(D=0,X=0,method="average"){
     D = D[-i,-i]
     n = n-1
   }
-  return(P)
+  return(list(P=P,coph=t))
 }
